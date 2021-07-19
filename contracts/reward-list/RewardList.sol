3e16d6a9b6c953fbde555c8cf37d8e7268c5a485
hnamzian <namazian87@gmail.com>
2021-07-20 00:24:00 +0300

    Create reward list and add get/create operations

--- contracts/reward-list/RewardList.sol
+++ contracts/reward-list/RewardList.sol
@@ -8,7 +8,17 @@ contract RewardList is Ownable {
   // period of time in seconds user must be rewarded proportionally
   uint256 _term;
 
+  // struct contains amount of reward and starting date for payment
+  struct Reward {
+    uint256 amount;
+    uint256 startTime;
+  }
+
+  // rewards must be paid to each user (address)
+  mapping(address => Reward) rewards;
+
   event Term(uint256);
+  event RewardAssigned(address indexed, uint256 amount, uint256 startTime);
 
   constructor(uint256 term_) {
     require(term_ > 0, "RewardList: term must be greater than 0!");
@@ -24,6 +34,24 @@ contract RewardList is Ownable {
 
     _term = term_;
     emit Term(_term);
+  }
+
+  /**
+   * @dev adds new address to rewards list and assigns a specified amount as its total amount
+   */
+  function addUserToRewardList(address user_, uint256 amount_) public onlyOwner {
+    require(rewards[user_].startTime == 0, "RewardList: User alreadt exists!");
+
+    Reward memory _reward = Reward(amount_, block.timestamp);
+    rewards[user_] = _reward;
+    emit RewardAssigned(user_, amount_, block.timestamp);
+  }
+
+  /**
+   * @dev returns total amount of rewards assigned to a specified address
+   */
+  function rewardsOf(address user_) public view returns (uint256) {
+    return rewards[user_].amount;
   }
 
 }
--- test/RewardList.test.js
+++ test/RewardList.test.js
@@ -32,3 +32,51 @@ describe("RewardList contract: Term", function () {
     }
   })
 });
+
+describe("RewardList: Rewards", () => {
+  it("Should add user to rewards list", async () => {
+    const [, user] = await ethers.getSigners();
+
+    const RewardList = await ethers.getContractFactory("RewardList");
+
+    const term = 1;
+    const rewardList = await RewardList.deploy(term);
+
+    const amount = 1;
+    await rewardList.addUserToRewardList(user.address, amount);
+
+    expect(await rewardList.rewardsOf(user.address)).to.equal(amount);
+  })
+  it("Should revert adding user to rewards list by unpermitted user", async () => {
+    const [, invalidOwner, user] = await ethers.getSigners();
+
+    const RewardList = await ethers.getContractFactory("RewardList");
+
+    const term = 1;
+    const rewardList = await RewardList.deploy(term);
+
+    const amount = 1;
+    try {
+      await rewardList.connect(invalidOwner).addUserToRewardList(user.address, amount);
+    } catch(ex) {
+      expect(ex.message).includes("revert");
+    }
+  })
+  it("Should revert adding existing user to rewards list", async () => {
+    const [user] = await ethers.getSigners();
+
+    const RewardList = await ethers.getContractFactory("RewardList");
+
+    const term = 1;
+    const rewardList = await RewardList.deploy(term);
+
+    const amount = 1;
+    await rewardList.addUserToRewardList(user.address, amount);
+
+    try {
+      await rewardList.addUserToRewardList(user.address, amount);
+    } catch(ex) {
+      expect(ex.message).includes("User alreadt exists");
+    }
+  })
+})
