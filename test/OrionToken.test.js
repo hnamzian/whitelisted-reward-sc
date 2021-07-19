c59455a63270adc93c5ee500be39ad77c8723b12
hnamzian <namazian87@gmail.com>
2021-07-19 11:02:54 +0300

    Test Granting Minter Role and mint function

--- test/OrionToken.test.js
+++ test/OrionToken.test.js
@@ -1,9 +1,9 @@
 const { expect } = require("chai");
 
-describe("Token contract", function () {
-  it("Deployment should assign name, symbol and totalSupply", async function () {
-    const [owner] = await ethers.getSigners();
+const MINTER_ROLE = ethers.utils.solidityKeccak256(['string'],["MINTER_ROLE"]);
 
+describe("Token contract", function () {
+  it("Deployment should assign name, symbol and totalSupply", async () => {
     const Token = await ethers.getContractFactory("OrionToken");
 
     const orionToken = await Token.deploy();
@@ -13,3 +13,36 @@ describe("Token contract", function () {
     expect(await orionToken.symbol()).to.equal("ORN");
   });
 });
+
+describe("Mint Token", function () {
+  it("Owner should grant MINTER_ROLE to minter address", async () => {
+    const [, minter] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("OrionToken");
+
+    const orionToken = await Token.deploy();
+
+    expect(await orionToken.hasRole(MINTER_ROLE, minter.address)).to.equal(false);
+
+    await orionToken.grantRole(MINTER_ROLE, minter.address);
+    
+    expect(await orionToken.hasRole(MINTER_ROLE, minter.address)).to.equal(true);
+  })
+  it("Minter should be able to mint token for any address", async () => {
+    const [, minter, receiver] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("OrionToken");
+
+    const orionToken = await Token.deploy();
+
+    await orionToken.grantRole(MINTER_ROLE, minter.address);
+
+    expect(await orionToken.balanceOf(receiver.address)).to.equal(0);
+
+    const amount = 1;
+    await orionToken.connect(minter).mint(receiver.address, amount);
+
+    const balance = await orionToken.balanceOf(receiver.address);
+    expect(parseInt(balance)).to.equal(amount);
+  })
+})
