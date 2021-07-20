3567d76cff0d347b49ae9cb4c612f4e99c4ffee4
hnamzian <namazian87@gmail.com>
2021-07-20 23:53:42 +0300

    Test getting reward while changing term

--- test/RewardPayout.test.js
+++ test/RewardPayout.test.js
@@ -92,4 +92,50 @@ describe("RewardPayout: Get reward", () => {
       expect(await rewardPayout.totalPayouts()).to.equal(totalPayment);
     }
   })
+  it("Should transfer ORN as rewards to user when term changes", async () => {
+    const [, user] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("OrionToken");
+
+    const totalSupply = 100;
+    const orionToken = await Token.deploy(totalSupply);
+
+    const RewardPayout = await ethers.getContractFactory("RewardPayout");
+
+    let term = 5;
+    const rewardPayout = await RewardPayout.deploy(term, orionToken.address);
+    
+    await orionToken.transfer(rewardPayout.address, 100);
+
+    const rewardAmount = 20;
+    await rewardPayout.addUserToRewardList(user.address, rewardAmount);
+
+    const rewardStartTime = await rewardPayout.rewardStartsAt(user.address);
+    let earned = 0;
+    while (earned < rewardAmount) {
+      const rate = rewardAmount / term;
+      await rewardPayout.connect(user).getReward();
+      earned = await rewardPayout.earned(user.address);
+      const balance = await orionToken.balanceOf(user.address);
+
+      const payments = await rewardPayout.paymentsOf(user.address);
+      const [, lastPaymentTime] = payments[payments.length - 1];
+
+      const totalPayment = Math.min(rate * (lastPaymentTime - rewardStartTime), 20);
+      expect(earned).to.equal(totalPayment);
+      expect(balance).to.equal(totalPayment); 
+      expect(await rewardPayout.totalPayouts()).to.equal(totalPayment);
+
+      term -= 1;
+      await rewardPayout.setTerm(term);
+    }
+
+    let reverted = false;
+    try {
+      await rewardPayout.connect(user).getReward();
+    } catch(ex) {
+      reverted = ex.message.includes("payout amount is 0")
+    }
+    expect(reverted).to.equal(true);
+  })
 })
